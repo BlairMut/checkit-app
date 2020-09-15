020777211470b72d269c64b2adb25b5199e2934f
BlairMut <blair.mutabaruka3@gmail.com>
2020-09-15 20:07:10 +1000

    adding add button auto login

--- checkitapp/app/src/main/java/com/example/checkit_app/MainActivity.java
+++ checkitapp/app/src/main/java/com/example/checkit_app/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.checkit_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton addBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+
+        addBtn = findViewById(R.id.add_button);
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddItem.class);
+                startActivity(intent);
+            }
+        });
     }
 }
