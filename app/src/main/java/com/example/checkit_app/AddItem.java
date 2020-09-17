78c0b5d00acf9ee5de859ddf71460d8ef77c61f9
dilum101 <dilumkumararathnayake.1993@gnmail.com>
2020-09-18 02:58:31 +1000

    add the google map to the activity_maps layout

---
+++ app/src/main/java/com/example/checkit_app/AddItem.java
@@ -0,0 +1,15 @@
+package com.example.checkit_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class AddItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_item);
+    }
+}
--- app/src/main/java/com/example/checkit_app/StartActivity.java
+++ app/src/main/java/com/example/checkit_app/StartActivity.java
@@ -7,13 +7,35 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+<<<<<<< HEAD
 public class StartActivity extends AppCompatActivity {
     Button login, register;
+=======
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class StartActivity extends AppCompatActivity {
+    Button login, register;
+
+    FirebaseUser firebaseUser;
+>>>>>>> add google map to the activity_maps layout
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
+<<<<<<< HEAD
+=======
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        //check if user is null
+        if(firebaseUser != null){
+            Intent intent = new Intent(StartActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+>>>>>>> add google map to the activity_maps layout
         login = findViewById(R.id.login);
         register = findViewById(R.id.register);
 
