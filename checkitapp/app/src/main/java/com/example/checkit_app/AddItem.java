99b6e075ea526867439dad3a8bab9eaac5818b66
BlairMut <blair.mutabaruka3@gmail.com>
2020-09-18 00:53:34 +1000

    Adding additem activity

--- checkitapp/app/src/main/java/com/example/checkit_app/AddItem.java
+++ checkitapp/app/src/main/java/com/example/checkit_app/AddItem.java
@@ -3,13 +3,69 @@ package com.example.checkit_app;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class AddItem extends AppCompatActivity {
+    EditText addItem;
+    Button addBtn;
+    String item;
+
+    ArrayList<String> arrayList;
+    ArrayAdapter<String> adapter;
+    ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Add Item");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        addItem = findViewById(R.id.addItem);
+        addBtn = findViewById(R.id.addBtn);
+
+
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AddItem.this, MainActivity.class);
+                item = addItem.getText().toString();
+                intent.putExtra("Value", item);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        lv = findViewById(R.id.listView_lv);
+
+        arrayList = new ArrayList<String>();
+        adapter = new ArrayAdapter<String>(AddItem.this, android.R.layout.simple_list_item_1, arrayList);
+        lv.setAdapter(adapter);
+        OnBtnClick();
+
+
+    }
+
+    public void OnBtnClick(){
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String result = addItem.getText().toString();
+                arrayList.add(result);
+                adapter.notifyDataSetChanged();
+            }
+        });
     }
 }
--- checkitapp/app/src/main/java/com/example/checkit_app/MainActivity.java
+++ checkitapp/app/src/main/java/com/example/checkit_app/MainActivity.java
@@ -6,13 +6,23 @@ import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
 
     FloatingActionButton addBtn;
+
+   /* ArrayList<String> arrayList;
+    ArrayAdapter<String> adapter;
+    ListView lv;
+    String item;*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +39,28 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, AddItem.class);
                 startActivity(intent);
+                //item = getIntent().getExtras().getString("Values");
+
             }
         });
+
+        /*lv = findViewById(R.id.listView_lv);
+
+        arrayList = new ArrayList<String>();
+        adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, arrayList);
+        lv.setAdapter(adapter);
+        OnBtnClick(item);*/
+
     }
+
+    /*public void OnBtnClick(final String st){
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String result = st;
+                arrayList.add(result);
+                adapter.notifyDataSetChanged();
+            }
+        });
+    }*/
 }
