70e6ce95f36a629cc2e71f05db9fa39623e17d3c
MilindRajpoot <rajpootmilind@gmail.com>
2020-10-05 14:49:39 +1100

    map working, geofence not getting created on basecamp it

--- checkitapp/app/src/main/java/com/example/checkit_app/MapsActivity.java
+++ checkitapp/app/src/main/java/com/example/checkit_app/MapsActivity.java
@@ -2,6 +2,7 @@ package com.example.checkit_app;
 
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
@@ -12,12 +13,23 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.GeofenceStatusCodes;
@@ -33,8 +45,15 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.Autocomplete;
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -48,6 +67,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private static int GEOFENCE_RADIUS_IN_METERS = 100; //(Note: Android does not recommend using a smaller radius than 100 meters as it cannot guarantee the accuracy.)
     private final static long GEOFENCE_EXPIRATION_IN_MILLISECONDS = Geofence.NEVER_EXPIRE;
     private PendingIntent geofencePendingIntent;
+
+
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String CORSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private static final String BACK_LOCATION = Manifest.permission.ACCESS_BACKGROUND_LOCATION;
+
+    private boolean mLocationPermissionGranted = false;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private MarkerOptions options;
+    private EditText searchText;
+    private ImageView mGps;
+    private Button baseCampLocationButton;
 
 // @Override
 
@@ -82,13 +113,68 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 //          moveTaskToBack(true);
-    }
+
+
+
+          searchText = (EditText)findViewById(R.id.input_search);
+          baseCampLocationButton = (Button)findViewById(R.id.buttonBaseCamp);
+          baseCampLocationButton.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View v) {
+
+
+                  LatLng currerntMarked = options.getPosition();
+//                  Log.d("TAG","Based Camped Location: "+ currerntMarked.latitude+" , "+currerntMarked.longitude);
+//                  Intent intent = new Intent(MapsActivity.this,MapsActivity.class);
+//                  intent.putExtra("latitude",currerntMarked.latitude);
+//                  intent.putExtra("longitude",currerntMarked.longitude);
+//                  startActivity(intent);
+                  myLat = currerntMarked.latitude;
+                  myLong = currerntMarked.longitude;
+
+              }
+          });
+
+          searchText.setFocusable(false);
+          mGps = (ImageView)findViewById(R.id.ic_gps);
+          searchText = (EditText)findViewById(R.id.input_search);
+          baseCampLocationButton = (Button)findViewById(R.id.buttonBaseCamp);
+          baseCampLocationButton.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View v) {
+
+
+                  LatLng currerntMarked = options.getPosition();
+                  Log.d("TAG","Based Camped Location: "+ currerntMarked.latitude+" , "+currerntMarked.longitude);
+                  addGeofence();
+
+
+              }
+          });
+          Places.initialize(getApplicationContext(),"AIzaSyBdcKjWCFvW4gUYxRoXSSYJjVrW0ypi-Wc");
+          searchText.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View v) {
+                  List<Place.Field> fieldList = Arrays.asList(Place.Field.ADDRESS, Place.Field.LAT_LNG,Place.Field.NAME);
+                  Intent intent = new Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY,fieldList).build(MapsActivity.this);
+                  Log.d("TAG","onClick - SearchText  Starting AutoComplete.....");
+                  startActivityForResult(intent,100);
+              }
+          });
+
+
+      }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
+            return;
+        }
         //once map is ready get user location to set marker & geofence
+        mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(false);
         initUserLocation();
     }
 
@@ -109,6 +195,36 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         //No permissions need to be requested continue app function
         Log.d(TAG, "Permissions passed setting location");
         getLastLocation();
+
+        Log.d("TAG","Initializing");
+
+        searchText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(actionId == EditorInfo.IME_ACTION_SEARCH
+                        || actionId == EditorInfo.IME_ACTION_DONE
+                        || event.getAction() == KeyEvent.ACTION_DOWN
+                        || event.getAction() == KeyEvent.KEYCODE_ENTER)
+                {
+
+                    Log.d("TAG","Seacrh Text LIstener");
+                    geoLocate();
+                }
+
+
+                return false;
+            }
+        });
+
+
+        mGps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("TAG","onClick: clicked gps icon");
+                getLastLocation();
+            }
+        });
+        //hideSoftKeyBoard();
 
     }
 
@@ -172,9 +288,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                             Log.d(TAG, "Lat: " + myLat + " Long: " + myLong);
                             // Add a marker to users Location
                             LatLng home = new LatLng(myLat, myLong);
-                            mMap.addMarker(new MarkerOptions().position(home).title("Marker At Home"));
+                            //mMap.addMarker(new MarkerOptions().position(home).title("Marker At Home"));
                             float zoomLevel = 16.0f; //This goes up to 21
-                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(home, zoomLevel));
+
+                            moveCamera(home,15f,"Your Location");
+
+                            //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(home, zoomLevel));
                             //add geofence around the home location
                             addGeofence();
                         } else {
@@ -262,6 +381,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     //add Circle to map that covers the area of the geofence
     private void addCircle(LatLng latLng) {
+        Log.d(TAG, "addCircle: flow enetring here");
         CircleOptions circleOptions = new CircleOptions();
         circleOptions.center(latLng);
         circleOptions.radius((float) MapsActivity.GEOFENCE_RADIUS_IN_METERS);
@@ -294,4 +414,64 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 //                    }
 //                });
 //    }
+
+
+
+
+    private void geoLocate()
+    {
+        Log.d("TAG","GeoLocating...");
+
+        String searchString = searchText.getText().toString();
+
+        Geocoder geocoder = new Geocoder(MapsActivity.this);
+        List<Address> list = new ArrayList<>();
+
+        try {
+            list = geocoder.getFromLocationName(searchString,1);
+        }catch(IOException e)
+        {
+            Log.e("TAG","geoLocate: IOException: "+e.getMessage());
+        }
+
+        if(list.size()>0)
+        {
+            Address address = list.get(0);
+            Log.d("TAG","geolocate: found a location "+ address.toString());
+            moveCamera(new LatLng(address.getLatitude(),address.getLongitude()),15f,address.getAddressLine(0));
+        }
+    }
+    private void moveCamera(LatLng latLng,float zoom,String title)
+    {
+        Log.d("TAG","moveCamera: moving camera to : lat"+latLng.latitude+" ,lng: "+latLng.longitude);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,zoom));
+
+
+        options = new MarkerOptions().position(latLng).title(title);
+        mMap.addMarker(options);
+        ;
+    }
+    private void hideSoftKeyBoard()
+    {
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN );
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Log.d("TAG","onClick - SearchText  Starting AutoComplete.....");
+        if(requestCode == 100 && resultCode == RESULT_OK)
+        {
+            Place place = Autocomplete.getPlaceFromIntent(data);
+            searchText.setText(place.getAddress());
+            geoLocate();
+        }
+        else
+        {
+            Status status = Autocomplete.getStatusFromIntent(data);
+            Log.e("TAG","AutoComplete Error: "+status.getStatusMessage());
+        }
+    }
+
 }
